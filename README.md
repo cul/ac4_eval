ebe6012a5cbe7f623a2c79ba06f72b0d91f7cfae
Benjamin Armintor <armintor@gmail.com>
2016-02-23 15:00:39 -0500

    base Rails and spec set up - fix typo in README
    - add secure token for slack notifications

---
+++ README.md
@@ -0,0 +1,5 @@
+== Academic Commons Sufia Evaluation
+
+This project is an evaluation of the Sufia 7 RC for the purpose of scoping customization work for the next major iteration of Academic Commons.
+
+See .travis.yml for CI config
---
+++ spec/spec_helper.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require 'rake'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'equivalent-xml/rspec_matchers'
+require 'byebug' unless ENV['TRAVIS']
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = File.expand_path("../fixtures", __FILE__)
+
+  config.use_transactional_fixtures = false
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.include Devise::TestHelpers, type: :controller
+  config.include EngineRoutes, type: :controller
+  config.include FactoryGirl::Syntax::Methods
+
+  config.infer_spec_type_from_file_location!
+end
+
+module FactoryGirl
+  def self.find_or_create(handle, by = :login)
+    tmpl = FactoryGirl.build(handle)
+    tmpl.class.send("find_by_#{by}".to_sym, tmpl.send(by)) || FactoryGirl.create(handle)
+  end
+end
