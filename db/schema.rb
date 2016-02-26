23520ba6e8559a98781ce5cc1a93da7c29d3dcad
Benjamin Armintor <armintor@gmail.com>
2016-02-26 18:57:49 -0500

    ACHYDRA-80 Wire CUL CAS/OmniAuth gem into the eval Sufia

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224182744) do
+ActiveRecord::Schema.define(version: 20160226183900) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -285,10 +285,14 @@ ActiveRecord::Schema.define(version: 20160224182744) do
     t.datetime "groups_last_update"
     t.string   "linkedin_handle"
     t.string   "orcid"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["uid"], name: "index_users_on_uid"
 
   create_table "version_committers", force: :cascade do |t|
     t.string   "obj_id"
---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Users::SessionsController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
